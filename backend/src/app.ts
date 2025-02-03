a459b401a49137da5f3c2a70f811fa4ea097d1d4
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-04 01:14:20 +0200

    BE: Add /api/auth/me

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import errorHandler from "./middleware/errorHandler";
 import routes from "./routes/index";
 import { apiResponse } from "./utils/apiResponse";
 import { connectToDb } from "./utils/dbConnect";
+import { auth } from "./middleware/auth";
 
 const app: Express = express();
 
@@ -23,6 +24,15 @@ app.use(cookieParser());
 
 // Initialize passport
 app.use(passport.initialize());
+
+// Test Routes
+app.get("/api/public", (_req, res) => {
+   res.json({ message: "This is a public endpoint" });
+});
+
+app.get("/api/protected", auth, (_req, res) => {
+   res.json({ message: "This is a protected endpoint" });
+});
 
 // Routes
 app.use("/api", routes);
