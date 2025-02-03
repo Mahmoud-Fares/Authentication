086da19f8ab0278227710c40b6fa97337bc53a75
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-02-04 01:08:47 +0200

    BE: Migrate to pnpm and make it generic for future auth libraries integration tests

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,5 @@
 import express, { Router } from "express";
 import userRouter from "./user.route";
-import threadRouter from "./thread.route";
 import authRouter from "./auth.route";
 import googleRouter from "./google.route";
 
@@ -10,6 +9,5 @@ router.use("/auth", authRouter);
 router.use("/auth/google", googleRouter);
 
 router.use("/users", userRouter);
-router.use("/threads", threadRouter);
 
 export default router;
